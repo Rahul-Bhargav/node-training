d31d505737200e1888ae1a9f8485ba0e99934382
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-01-22 01:28:38 +0530

    Refactored  Every Some excercise

--- functional-javascript/every-some.js
+++ functional-javascript/every-some.js
@@ -1,21 +1,16 @@
-
-
-
 function checkUsersValid(goodUsers) {
-    return function allUsersValid(submittedUsers) {
-
+    var allUsersValid = function (submittedUsers) {
         var result = false;
-
         result = submittedUsers.every(function isValid(submittedUser) {
             var validity = false;
-            validity = goodUsers.some(function isPresent(goodUser){
-                return goodUser === submittedUser;
+            validity = goodUsers.some( function isPresent(goodUser){
+                return goodUser.id === submittedUser.id;
             });
             return validity;
         });
-
         return result;
     };
+    return allUsersValid;
 }
 
 module.exports = checkUsersValid
