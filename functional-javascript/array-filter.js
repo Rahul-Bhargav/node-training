4532b187bbee0d8940bb66db85b7a0135aa14427
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-01-22 18:58:16 +0530

    Refactored Array Filter file

--- functional-javascript/array-filter.js
+++ functional-javascript/array-filter.js
@@ -1,4 +1,4 @@
-function isLessThan50(object){
+function isShortMessage(object){
         return object.message.length < 50;
 }
 
@@ -7,7 +7,7 @@ function getMessageElement (object){
 }
 
 function getShortMessages(messages){
-    var filteredMessages = messages.filter(isLessThan50);
+    var filteredMessages = messages.filter(isShortMessage);
     var messageArray = filteredMessages.map(getMessageElement);
     return messageArray;
 }
