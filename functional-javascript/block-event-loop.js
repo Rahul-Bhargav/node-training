1eb05fc4d037ccda660d15793e25eb5aa7826686
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-01-24 23:25:47 +0530

    Block Event Loop excercise in functional JavaScript

--- functional-javascript/block-event-loop.js
+++ functional-javascript/block-event-loop.js
@@ -1,7 +1,17 @@
 function repeat(operation, num) {
-    if (num <= 0) return
-    operation()
-    return repeat(operation, --num)
+    if (num <= 0) return;
+
+    operation();
+
+    if (num % 10 === 0) {
+        setTimeout(function () {
+            return repeat(operation, --num);
+        });
+    }
+    else {
+        return repeat(operation, --num);
+    }
+
 }
 
 module.exports = repeat
