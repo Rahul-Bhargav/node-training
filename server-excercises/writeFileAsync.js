89a209ddb3604055d67dc8d9b4ae740419f6b700
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-02-06 14:07:47 +0530

    Add Write file Async

---
+++ server-excercises/writeFileAsync.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+
+function fileWrite (inputPath, text) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(inputPath, text, (err) => {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    })
+  })
+}
+
+function checkFileValidity (inputPath) {
+  return new Promise((resolve, reject) => {
+    if (typeof inputPath !== 'string') {
+      reject('Enter a valid File Path')
+    }
+    fs.stat(inputPath, (err, stats) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          reject('No file at', inputPath)
+
+          return
+        }
+        reject(err)
+
+        return
+      }
+      if (stats.isDirectory()) {
+        reject('The input path is a Directory')
+      }
+      // If file size is greater than 100MB
+      if (stats.size > 104857600) {
+        reject('The file size exceeds 100MB')
+      }
+      if (!(path.extname(inputPath) === '.txt')) {
+        reject('The input path is not txt')
+      }
+      resolve()
+    })
+  })
+}
+
+function writeFileAsync (inputPath, text) {
+  return new Promise((resolve, reject) => {
+    checkFileValidity(inputPath)
+      .then(() => fileWrite(inputPath, text))
+      .then(() => {
+        resolve()
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+module.exports = writeFileAsync
