ff04ac83f7a5257f3c48ec24fec0666d34971460
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-02-09 16:01:55 +0530

    fix DB Queries

--- server-excercises/dabataseInterface.js
+++ server-excercises/dabataseInterface.js
@@ -4,22 +4,22 @@ const sequelize = new Sequelize('postgres://rahulsurabhi:rah1161!@localhost:5432
 
 const databaseOprations = {
   insert: function (descriptionInput) {
-    return sequelize.query(`INSERT INTO TASKS (DESCRIPTION) VALUES (:description)`, { replacements: {description: descriptionInput}, type: sequelize.QueryTypes.SELECT })
+    return sequelize.query(`INSERT INTO TASKS (DESCRIPTION) VALUES (:description) RETURNING ID`, { replacements: { description: descriptionInput }, type: sequelize.QueryTypes.SELECT })
   },
   read: function () {
     return sequelize.query(`SELECT ID,DESCRIPTION,STATUS FROM TASKS ORDER BY ID ASC`, { replacements: {}, type: sequelize.QueryTypes.SELECT })
   },
   update: function (descriptionInput, idInput, statusInput) {
     if (!descriptionInput) {
-      return sequelize.query(`UPDATE TASKS SET STATUS = :status WHERE ID = :id`, { replacements: {status: statusInput, id: idInput}, type: sequelize.QueryTypes.SELECT })
+      return sequelize.query(`UPDATE TASKS SET STATUS = :status WHERE ID = :id`, { replacements: { status: statusInput, id: idInput }, type: sequelize.QueryTypes.UPDATE })
     }
     if (!statusInput) {
-      return sequelize.query(`UPDATE TASKS SET DESCRIPTION = :description WHERE ID = :id`, { replacements: {description: descriptionInput, id: idInput}, type: sequelize.QueryTypes.SELECT })
+      return sequelize.query(`UPDATE TASKS SET DESCRIPTION = :description WHERE ID = :id`, { replacements: { description: descriptionInput, id: idInput }, type: sequelize.QueryTypes.UPDATE })
     }
-    return sequelize.query(`UPDATE TASKS SET DESCRIPTION = :description, STATUS = :status WHERE ID = :id`, { replacements: {status: statusInput, description: descriptionInput, id: idInput}, type: sequelize.QueryTypes.SELECT })
+    return sequelize.query(`UPDATE TASKS SET DESCRIPTION = :description, STATUS = :status WHERE ID = :id`, { replacements: { status: statusInput, description: descriptionInput, id: idInput }, type: sequelize.QueryTypes.UPDATE })
   },
   destroy: function (idInput) {
-    return sequelize.query(`DELETE FROM TASKS WHERE ID=:id`, { replacements: {id: idInput}, type: sequelize.QueryTypes.SELECT })
+    return sequelize.query(`DELETE FROM TASKS WHERE ID=:id`, { replacements: { id: idInput }, type: sequelize.QueryTypes.DELETE })
   }
 }
 
